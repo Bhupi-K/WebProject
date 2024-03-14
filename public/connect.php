ca2bf7949d69a299c8fc88ea7203cf8bf56d2dce
Bhupi-K <Kumarbhupinder0201@gmail.com>
2024-03-13 21:57:20 -0700

    Updated files to be suited for usage

--- public/connect.php
+++ public/connect.php
@@ -8,18 +8,21 @@
 
 	// Database connection
 	$conn = new mysqli('localhost','root','','test');
-	if($conn->connect_error){
+	// $mysqli = new mysqli("localhost", $username, $password, $database);
+	if($conn->connect_error)
+		{
 		echo "$conn->connect_error";
-		die("Connection Failed : ". $conn->connect_error);
-	} else {
-		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssssi", $firstName, $lastName, $gender, $email, $password, $number);
+		die("Not working : ". $conn->connect_error);
+		} 
+	else {
+		$stmt = $conn->prepare("insert into registration(fullName, course, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssi", $fullName, $course, $gender, $email, $password, $number);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Registration successfully...";
 		$stmt->close();
 		$conn->close();
-	}
+		}
 ?>
 
 
